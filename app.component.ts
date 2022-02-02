e59c6ed60233a988e711cde5bdefdec120dafac2
jxnetapps <sampathraj.indian@gmail.com>
2022-02-03 00:13:23 +0530

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { FileManagerComponent, NavigationPaneService, ToolbarService, DetailsViewService } from '@syncfusion/ej2-angular-filemanager';
+/**
+ * File Manager full functionalities sample
+ */
+@Component({
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
+    encapsulation: ViewEncapsulation.None,
+	providers: [ NavigationPaneService, ToolbarService, DetailsViewService]
+})
+
+export class AppComponent {
+    public ajaxSettings: object;
+    public view: string;
+    public hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
+    public ngOnInit(): void {
+        this.ajaxSettings = {
+            url: this.hostUrl + 'api/FileManager/FileOperations',
+            getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
+            uploadUrl: this.hostUrl + 'api/FileManager/Upload',
+            downloadUrl: this.hostUrl + 'api/FileManager/Download'
+        };
+       this.view = "Details";
+       }
+}
